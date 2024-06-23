173f6b25f48f2c4d994ebe81491cb02771d10fff
Ziggy-dst <542335894@qq.com>
2024-06-23 12:48:50 -0400

    NEW: level objective detector added

---
+++ MiniJam-161-Style/Assets/Scripts/Scriptable Objects/LevelObjectivesSO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scriptable_Objects
+{
+    [CreateAssetMenu(fileName = "LevelObjectivesSO", menuName = "ScriptableObjects/LevelObjectivesSO", order = 0)]
+    public class LevelObjectivesSO : ScriptableObject
+    {
+        [Serializable]
+        public struct LevelObjective
+        {
+            public List<string> winningObjectiveTags;
+            public List<string> failingObjectiveTags;
+        }
+
+        public List<LevelObjective> LevelObjectives;
+    }
+}
